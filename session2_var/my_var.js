01aa908438f377d674c58f7aea17365c2863133d
Hung Nguyen Xuan <hungnx@pixelz.com>
2019-07-26 19:07:29 +0700

    session 3

---
+++ session2_var/my_var.js
@@ -0,0 +1,77 @@
+// let y = "Hello world";
+// let Y = "Welcome";
+
+// let xinChao = 'Xin chao';
+// let XinChao = 'Chao cac ban';
+
+// var welcome = "Welcome to C4E JS 41";
+
+/*
+console.log(xinChao);
+console.log(XinChao);
+
+console.log(y);
+*/
+
+// const pi = 3.14; // Khai bao hang so Pi
+// // pi = 5;
+
+// let X = 5;
+// // console.log(X);
+// X = 10;
+// console.log(X);
+
+// var y = 20;
+// var z = X + y;
+// var i = X * y;
+// console.log("Gia tri" + " cua z la: " + z);
+// console.log("Gia tri cua i la: " + i);
+
+//Pham vi bien
+// var x = 50;
+// var m = 100;
+// {
+    // let x = 20;
+    // console.log(x);
+
+    // var y = "10";
+    // console.log(y);
+
+    // var m = 10;
+    // console.log(m);
+// }
+// console.log(x);
+// console.log(y);
+// console.log(m);
+
+
+// let x = 100;
+// {
+//     var x = 10;
+//     console.log(x);
+// }
+// console.log(x);
+
+// => 
+// var x;
+// let x = 100;
+// {
+//     x = 10;
+// }
+// console.log(x);
+
+x = 100;
+console.log(x);
+// var x;
+let x;
+
+
+
+
+
+
+
+
+
+
+
---
+++ session3_datatypes/datatypes_intro.js
@@ -0,0 +1,21 @@
+// let x = 100;
+// let y = "100";
+
+// console.log(x);
+// console.log(y);
+// console.log(`Gia tri cua x la: ${x}`);
+// let a = 10;
+// let b = 20;
+// console.log(`Tong cua ${a} va ${b} la ${a + b}`);
+
+// var x = 100;
+// console.log(x);
+
+let name = prompt("Bạn tên là gì?")
+let age = prompt("Bạn bao nhiêu tuổi?");
+console.log(`Ban ten la ${name}, ban ${age} tuoi`);
+
+let input = prompt("Tên và tuổi của bạn là gì?");
+console.log(`Ban ten la ${input}, ban ${input} tuoi`);
+
+
